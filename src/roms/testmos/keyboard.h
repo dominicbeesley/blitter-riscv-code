1359846cd54f6d5dc152d4d5c7dcec3984edf915
dominicbeesley <dominicbeesley@gmail.com>
2024-12-14 13:46:25 +0000

    test mos now just does splash and simple memory test

--- src/roms/testmos/keyboard.h
+++
@@ -1,8 +0,0 @@
-#ifndef __KEYBOARD_H__
-#define __KEYBOARD_H__
-
-void call_KEYV_NVC();
-void call_KEYV_NV();
-
-
-#endif
--- src/roms/testmos/moslib.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MOSLIB_H__
-#define __MOSLIB_H__
-
-#include <stdint.h>
-
-extern void mos_oswrch(unsigned char vdu);
-
-#endif
